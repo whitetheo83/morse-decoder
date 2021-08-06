b628eb77c0414c23fb177a5b9b9631aec4eed91e
Evgeni Bochkov <60480912+whitetheo83@users.noreply.github.com>
2021-08-06 16:17:15 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -39,8 +39,35 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let numArr = expr.match(/.{1,10}/g);
+    let x ='';
+
+    for(let i = 0; i < numArr.length; i++) {
+        if (numArr[i] == '**********') {
+            x = x + ' ';
+        } else {
+        let charArr = numArr[i].match(/.{1,2}/g);
+        let c = '';
+            for (j = 0; j <charArr.length; j++) {
+                if (charArr[j] == '10') {
+                    c = c + '.';
+                } else if (charArr[j] == '11') {
+                    c = c + '-';
+                } else {
+                    continue;
+                }
+            }
+            for (let key in MORSE_TABLE) {
+                if (key == c) {
+                    x = x + MORSE_TABLE[key];
+                } 
+            }
+        }
+    }
+    return (x);
 }
 
-module.exports = {
+   module.exports = {
     decode
-}
+}   
+//console.log(decode('0010101010**********00001011110000111111000010111000101110100000111010'));
